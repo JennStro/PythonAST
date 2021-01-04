3c361e3e86126b4d9e0baf862165929cb5f1d51b
JennStro <jennys199@gmail.com>
2021-01-04 19:00:09 +0000

    Print out correct return type

--- AST.py
+++ AST.py
@@ -2,7 +2,7 @@ import ast
 
 class NodeVisitor(ast.NodeVisitor):
     def visit_Constant(self, node: ast.Constant):
-        print("Int " + str(node.s))
+        print(str(node.s))
 
     def visit_BinOp(self, node: ast.BinOp):
         print("Binop")
@@ -10,7 +10,17 @@ class NodeVisitor(ast.NodeVisitor):
 
     def visit_FunctionDef(self, node: ast.FunctionDef):
         returnValue = node.body[0].value.value
-        print("public " + str(type(returnValue)) + " " + str(node.name))
+        returnType = type(returnValue)
+        print(returnType)
+        if returnType is int:
+            print("public int " + str(node.name))
+        elif returnType is str:
+            print("public String " + str(node.name))
+        elif returnType is float:
+            print("public double " + str(node.name))
+        else:
+            print("public void " + str(node.name))
+
         self.generic_visit(node)
 
 
@@ -20,8 +30,9 @@ NodeVisitor().visit(simpletree)
 print(ast.dump(simpletree))
 
 
-treeWithFunction = ast.parse("""def f(): 
-    return 1""")
+treeWithFunction = ast.parse("""def f():
+    return 'helo'
+    """)
 
 NodeVisitor().visit(treeWithFunction)
 print(ast.dump(treeWithFunction))
