45f1a751d9ea6d32fd47d091235405553c9e6b12
JennStro <jennys199@gmail.com>
2021-01-04 14:30:12 +0000

    Print constant

--- AST.py
+++ AST.py
@@ -1,5 +1,10 @@
 import ast
 
+class NodeVisitor(ast.NodeVisitor):
+    def visit_Constant(self, node: ast.Constant):
+        print("Int " + str(node.s))
+
 tree = ast.parse("1 + 2")
 
+NodeVisitor().visit(tree)
 print(ast.dump(tree))
