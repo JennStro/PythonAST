476df04d8daa7c2de35db4944a157e090219dd6c
JennStro <jennys199@gmail.com>
2021-01-04 14:47:00 +0000

    Visit children

--- AST.py
+++ AST.py
@@ -4,6 +4,10 @@ class NodeVisitor(ast.NodeVisitor):
     def visit_Constant(self, node: ast.Constant):
         print("Int " + str(node.s))
 
+    def visit_BinOp(self, node: ast.BinOp):
+        print("Binop")
+        self.generic_visit(node)
+
 tree = ast.parse("1 + 2")
 
 NodeVisitor().visit(tree)
