1471cacce236a835311c999ea8e45dd0d4805ff3
JennStro <jennys199@gmail.com>
2021-01-04 20:49:18 +0000

    Add a test

--- AST.py
+++ AST.py
@@ -5,35 +5,26 @@ class NodeVisitor(ast.NodeVisitor):
     def __init__(self):
         self.javaProgram = ""
 
-    def visit_Constant(self, node: ast.Constant):
-        print(str(node.s))
-
     def visit_FunctionDef(self, node: ast.FunctionDef):
-        returnValue = node.body[0].value.value
-        returnType = type(returnValue)
-        print(returnType)
-        if returnType is int:
-            self.javaProgram += "public int " + str(node.name)
-        elif returnType is str:
-            self.javaProgram += "public String " + str(node.name)
-        elif returnType is float:
-            self.javaProgram += "public double " + str(node.name)
+        lastStatementInBody = node.body[-1]
+        if isinstance(lastStatementInBody, ast.Return):
+            returnValue = lastStatementInBody.value.value
+            returnType = type(returnValue)
+            print(returnType)
+            if returnType is int:
+                self.javaProgram += "public int " + str(node.name)
+            elif returnType is str:
+                self.javaProgram += "public String " + str(node.name)
+            elif returnType is float:
+                self.javaProgram += "public double " + str(node.name)
         else:
             self.javaProgram += "public void " + str(node.name)
 
         self.generic_visit(node)
 
 
-
-simpletree = ast.parse("1 + 2")
-
-print(ast.dump(simpletree))
-NodeVisitor().visit(simpletree)
-
-
-
-
 treeWithFunction = ast.parse("""def f():
+    y = 1
     return 1
     """)
 
@@ -41,3 +32,13 @@ print(ast.dump(treeWithFunction))
 visitor = NodeVisitor()
 visitor.visit(treeWithFunction)
 print(visitor.javaProgram)
+
+
+def test_function_returns_integer():
+    visitor = NodeVisitor()
+    visitor.visit(treeWithFunction)
+    assert visitor.javaProgram == "public int f", "Should be: public int f, was " + visitor.javaProgram
+
+if __name__ == "__main__":
+    test_function_returns_integer()
+    print("Everything ok.")
